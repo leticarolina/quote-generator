a661fc44db4ae674580e6e99dcdabe6c9b7e2add
Leticia Azevedo <109385942+leticarolina@users.noreply.github.com>
2023-03-14 22:07:24 +0800

    adding quotes to the project

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello")
+let apiQuotes = [];
+
+//Get quotes from API
+//created a function called GetQuotes
+async function getQuotes() {
+  //logged the api inside the function thru a variable
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    //response and apiQuotes are variables declared
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //error here
+  }
+}
+
+//on load , to get the quotes running as soon as the page loads
+getQuotes();
+
+//Show a new quote
+function newQuote() {
+  //code to pick a random quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
